b651e4c082be64060fac414a42679c380eb90d27
中村駿輝 <shnklife@gmail.com>
2020-06-03 16:39:33 +0900

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("sample"),
+      ),
+      body: Center(
+          child: Text("hello world!")
+      ),
+    );
+  }
+}
